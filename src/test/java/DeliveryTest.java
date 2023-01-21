ac114e3a5afa8816659389fde03cdebe62192065
Fedor Sharov <1710193@mail.ru>
2023-01-21 13:21:40 +0300

    Check CI - happyPath

---
+++ src/test/java/DeliveryTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DeliveryTest {
+
+    private String actualDate(long daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeAll
+    static void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void checkAppeyorHappy() {
+        //Configuration.holdBrowserOpen = true;
+
+        $("[data-test-id='city'] input").sendKeys("Казань");
+        $("[data-test-id='date'] input").sendKeys(actualDate(3, "dd MM yyyy"));
+        $("[data-test-id='name'] input").sendKeys("Иван Иванов");
+        $("[data-test-id='phone'] input").sendKeys("+79991112233");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Запланировать")).click();
+
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Успешно!"))
+                .shouldHave(visible);
+    }
+}
