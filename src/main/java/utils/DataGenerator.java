1348ab74e86fbfb6c9563263954ba8963e53bad5
Fedor Sharov <1710193@mail.ru>
2023-01-23 06:23:50 +0300

    Finished task 1

--- src/main/java/utils/DataGenerator.java
+++ src/main/java/utils/DataGenerator.java
@@ -1,4 +1,56 @@
 package utils;
 
+import com.github.javafaker.Faker;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static Faker faker;
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        faker = new Faker(new Locale(locale));
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        faker = new Faker(new Locale(locale));
+        return faker.name().name();
+    }
+
+    public static String generatePhone(String locale) {
+        faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().cellPhone();
+    }
+
+    public static class Registration {
+
+        private Registration() {
+            String city;
+            String name;
+            String phone;
+        }
+
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+
 }
--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,8 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+import utils.DataGenerator;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -17,24 +20,51 @@ public class DeliveryTest {
         return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
     }
 
     @Test
-    void checkAppeyorNotHappy() {
-        //Configuration.holdBrowserOpen = true;
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        Configuration.holdBrowserOpen = true;
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 6;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
-        $("[data-test-id='city'] input").sendKeys("Казань");
-        $("[data-test-id='date'] input").sendKeys(actualDate(3, "dd MM yyyy"));
-        $("[data-test-id='name'] input").sendKeys("IVAN AVANOV");
-        $("[data-test-id='phone'] input").sendKeys("+79991112233");
+        $("[data-test-id='city'] input").sendKeys(DataGenerator.generateCity("ru"));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id='name'] input").sendKeys(validUser.getName());
+        $("[data-test-id='phone'] input").sendKeys(DataGenerator.generatePhone("ru"));
         $("[data-test-id='agreement']").click();
         $$("button").findBy(text("Запланировать")).click();
 
+        while ($("[data-test-id='city'] .input__sub").has(text("Доставка в выбранный город недоступна"))) {
+            $("[data-test-id='city'] input").doubleClick().sendKeys(Keys.DELETE);
+            $("[data-test-id='city'] input").sendKeys(DataGenerator.generateCity("ru"));
+            $$("button").findBy(text("Запланировать")).click();
+        }
         $("[data-test-id='success-notification']")
                 .shouldHave(Condition.text("Успешно!"))
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldHave(visible);
+
+        $("[data-test-id='date'] input").doubleClick().sendKeys(secondMeetingDate);
+        $$("button").findBy(text("Запланировать")).click();
+        $("[data-test-id='replan-notification']")
+                .shouldHave(Condition.text("Необходимо подтверждение"))
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldHave(visible);
+
+        $$("button").findBy(text("Перепланировать")).click();
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Успешно!"))
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldHave(visible);
+
     }
+
 }
